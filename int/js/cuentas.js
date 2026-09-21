c97062f00100265aec143e53f8dc934965f76acd
servicomplus <servicomplus2009@gmail.com>
2026-09-20 21:26:03 -0500

    Add files via upload

---
+++ int/js/cuentas.js
@@ -0,0 +1,42 @@
+const MIS_CUENTAS = [
+  {
+    banco: "BCP",
+    titular: "ServiComp Plus",
+    cuenta: "191-XXXXXXXX-0-XX",
+    cci: "002-191-XXXXXXXXXXXX-XX",
+    telefono: "",
+    color: "#002a66",
+    logo: "logo.png",
+    qr: "" // Deja vacío "" si no tiene QR, o pon la ruta a la imagen si tiene
+  },
+  {
+    banco: "BBVA",
+    titular: "ServiComp Plus",
+    cuenta: "0011-XXXX-XXXXXXXXXX",
+    cci: "011-XXX-XXXXXXXXXXXXXXXX-XX",
+    telefono: "",
+    color: "#004481",
+    logo: "",
+    qr: ""
+  },
+  {
+    banco: "Interbank",
+    titular: "ServiComp Plus",
+    cuenta: "200-XXXXXXXXXX",
+    cci: "003-200-XXXXXXXXXXXX-XX",
+    telefono: "",
+    color: "#009946",
+    logo: "",
+    qr: ""
+  },
+  {
+    banco: "Yape / Plin",
+    titular: "ServiComp Plus",
+    cuenta: "",
+    cci: "",
+    telefono: "973952322",
+    color: "#6c1382",
+    logo: "",
+    qr: ""
+  }
+];
